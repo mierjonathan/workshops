1c8f5b8796170d49f3d2c3650e70d78929640de5
Jonathan Mier <jonathanmier95@gmail.com>
2021-12-07 16:48:22 -0700

    Inspec tests have been added and are passing in test kitchen to jmier_mongodb and jmier_tomcat

--- solutions/cookbooks/jmier_mongodb/recipes/yum_repo.rb
+++ solutions/cookbooks/jmier_mongodb/recipes/yum_repo.rb
@@ -4,7 +4,7 @@
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
 
-# Creates Yum repo for mongodb 
+# Creates Yum repo for mongodb
 yum_repository node['jmier_mongodb']['yum_repo']['repo_id'] do
   baseurl node['jmier_mongodb']['yum_repo']['baseurl']
   gpgcheck node['jmier_mongodb']['yum_repo']['gpgcheck']
